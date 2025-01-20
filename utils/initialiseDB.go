bcfa939311193816a44d5b87d8ef9356da6abd97
bshisia <shisiabrian7@gmail.com>
2025-01-20 15:16:34 +0300

    feat: create a comments table with the required entries

--- utils/initialiseDB.go
+++ utils/initialiseDB.go
@@ -53,5 +53,25 @@ func InitialiseDB() *sql.DB {
 		return nil
 	}
 
+	_, err = db.Exec(`
+	CREATE TABLE IF NOT EXISTS comments (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        post_id INTEGER,
+        user_id INTEGER,
+        content TEXT NOT NULL,
+        comment_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+        likes INTEGER DEFAULT 0,
+        dislikes INTEGER DEFAULT 0,
+        FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
+        FOREIGN KEY (user_id) REFERENCES users(id)
+    );
+    CREATE INDEX IF NOT EXISTS idx_comments_post_id ON comments(post_id);
+    CREATE INDEX IF NOT EXISTS idx_comments_user_id ON comments(user_id);
+	`)
+	if err != nil {
+		fmt.Errorf("Failed to create comments table: %v", err)
+		return nil
+	}
+
 	return db
 }

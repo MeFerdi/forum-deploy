7538effa9e213536b7a16b0260fdc735226b20f3
bshisia <shisiabrian7@gmail.com>
2025-01-23 13:17:22 +0300

    chore: handle username validation

--- handlers/signuphandler.go
+++ handlers/signuphandler.go
@@ -4,22 +4,21 @@ import (
 	"database/sql"
 	"html/template"
 	"net/http"
-)
 
+	_ "github.com/mattn/go-sqlite3")
 
 type SignUpErrors struct {
-    NameError     string
-    EmailError    string
-    UsernameError string
-    PasswordError string
-    GeneralError  string
+	NameError     string
+	EmailError    string
+	UsernameError string
+	PasswordError string
+	GeneralError  string
 }
 
 type SignUpData struct {
 	Errors   SignUpErrors
-	Name     string
 	Email    string
-	Username string
+	UserName string
 }
 
 var GlobalDB *sql.DB
@@ -31,7 +30,22 @@ func InitDB(database *sql.DB) {
 func SignUpHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
 		tmpl := template.Must(template.ParseFiles("templates/signup.html"))
-		tmpl.Execute(w, &SignUpData)
+		tmpl.Execute(w, &SignUpData{})
 		return
+	}
+
+	if r.Method == "POST" {
+		data := SignUpData{
+			UserName: r.FormValue("username"),
+			Email:    r.FormValue("email"),
+		}
+		errors := SignUpErrors{}
+		hasError := false
+
+		// Validate name
+		if data.UserName == "" {
+			errors.UsernameError = "UserName is required"
+			hasError = true
+		}
 	}
 }

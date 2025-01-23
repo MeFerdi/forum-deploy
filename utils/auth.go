4b143a82cfa4fef81ddd151809907d9a8e873c49
bshisia <shisiabrian7@gmail.com>
2025-01-23 12:30:45 +0300

    feat: add an email validation to ensure the email is in its correct format

--- utils/auth.go
+++ utils/auth.go
@@ -1,6 +1,10 @@
 package utils
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"regexp"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
@@ -10,4 +14,10 @@ func HashPassword(password string) (string, error) {
 func CheckPasswordsHash(password, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
+}
+
+func ValidateEmail(email string) bool {
+	pattern := `^[a-zA-Z._%+-]+@[a-zA-Z.-]+\.[a-zA-Z]{2,}$`
+	match, _ := regexp.MatchString(pattern, email)
+	return match
 }

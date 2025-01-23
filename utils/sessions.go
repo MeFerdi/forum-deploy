a86d4201776c0aa33646ce3401f2333ba7dd5de1
bshisia <shisiabrian7@gmail.com>
2025-01-23 16:01:32 +0300

    chore: create a function to create a sessio for each user

--- utils/sessions.go
+++ utils/sessions.go
@@ -1,7 +1,9 @@
 package utils
 
 import (
+	"database/sql"
 	"encoding/base64"
+	"time"
 
 	"golang.org/x/exp/rand"
 )
@@ -10,4 +12,18 @@ func GenerateSessionToken() string {
 	b := make([]byte, 32)
 	rand.Read(b)
 	return base64.URLEncoding.EncodeToString(b)
+}
+
+func CreateSessions(db *sql.DB, userID string) (string, error) {
+	SessionToken := GenerateSessionToken()
+	ExpiresAt := time.Now().Add(24 * time.Hour)
+	_, err := GlobalDB.Exec(`
+	INSERT INTO sessions(id, user_id, expires_at)
+	VALUES (?, ?, ?)
+	`, SessionToken, userID, ExpiresAt)
+	if err != nil {
+		return "", err
+	}
+
+	return SessionToken, nil
 }

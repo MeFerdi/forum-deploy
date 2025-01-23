3e79e7e6c8bb5d4e3f740a970d4d50a91fce6008
bshisia <shisiabrian7@gmail.com>
2025-01-23 15:46:14 +0300

    chore: validate the input is provided

--- handlers/signinhandler.go
+++ handlers/signinhandler.go
@@ -1,6 +1,9 @@
 package handlers
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
 
 type SignInData struct {
 	Username      string
@@ -10,5 +13,33 @@ type SignInData struct {
 }
 
 func SignInHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("template/signin.html"))
 
+	if r.Method == "GET" {
+		tmpl.Execute(w, nil)
+		return
+	}
+
+	if r.Method == "POST" {
+		data := SignInData{}
+
+		// get form values
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+
+		// validate input
+		if username == "" {
+			data.UsernameError = "Username is required"
+		}
+
+		if password == "" {
+			data.PasswordError = "password is required"
+		}
+
+		if data.UsernameError != "" || data.PasswordError != "" {
+			data.Username = username
+			tmpl.Execute(w, data)
+			return
+		}
+	}
 }

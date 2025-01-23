6ddc1fbd7a61207c2f0c0b6fe9356088bed016bd
bshisia <shisiabrian7@gmail.com>
2025-01-23 13:10:06 +0300

    create structs for sign up data

--- handlers/signuphandler.go
+++ handlers/signuphandler.go
@@ -1,6 +1,17 @@
 package handlers
 
-import "database/sql"
+import (
+	"database/sql"
+	"html/template"
+	"net/http"
+)
+
+type SignUpData struct {
+	Errors   SignUpErrors
+	Name     string
+	Email    string
+	Username string
+}
 
 var GlobalDB *sql.DB
 
@@ -8,3 +19,10 @@ func InitDB(database *sql.DB) {
 	GlobalDB = database
 }
 
+func SignUpHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		tmpl := template.Must(template.ParseFiles("templates/signup.html"))
+		tmpl.Execute(w, &SignUpData)
+		return
+	}
+}

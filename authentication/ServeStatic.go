0f445139f97e3e014e25f898ef11b1d6bfacec8c
somotto <stephenonyango4783@gmail.com>
2025-02-12 17:19:49 +0300

    handle static files

---
+++ authentication/ServeStatic.go
@@ -0,0 +1,28 @@
+package handlers
+
+import (
+	"net/http"
+	"os"
+
+	"forum/utils"
+)
+
+// Serve static files and avoid listing them to the web page
+func ServeStatic(w http.ResponseWriter, r *http.Request) {
+	file := "." + r.URL.Path
+
+	info, err := os.Stat(file)
+	if err != nil {
+		// file is not in the directory
+		utils.RenderErrorPage(w, http.StatusNotFound, http.StatusText(http.StatusNotFound))
+		return
+	}
+
+	if info.IsDir() {
+		// if it is a directory
+		utils.RenderErrorPage(w, http.StatusNotFound, http.StatusText(http.StatusNotFound))
+		return
+	}
+
+	http.ServeFile(w, r, file)
+}
